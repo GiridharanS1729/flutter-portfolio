4b5d65388303c0fad018473e0ec4a7ff437c6435
Giridharan S <117799506+GiridharanS1729@users.noreply.github.com>
2024-12-06 03:54:55 +0530

    modified oasis to devskillhub 061220240354

--- lib/models/Expr.dart
+++ lib/models/Expr.dart
@@ -6,34 +6,34 @@ class Expr {
 
 final List<Expr> demoExperience = [
   Expr(
-    titel: "Web Development",
-    company: "Oasis Infobyte",
+    titel: "Java FullStack Developer Intern",
+    company: "Dev Skill Hub",
     durs:
-        "Jul 2024 - Aug 2024· 2 mos          Remote",
+        "Aug 2024 - Sep 2024· 2 mos          Remote",
   ),
   Expr(
-    titel: "Full-Stack Development",
+    titel: "FullStack Developer Intern",
     company: "Cognifyz Technologies",
     durs:
-        "Jun 2024 - Jul 2024 · 2 mos          Remote",
+        "Jun 2024 - Jul 2024 · 1 mos          Remote",
   ),
   Expr(
     titel: "UI/UX Intern",
     company: "Cognifyz Technologies",
     durs:
-        "Apr 2024 - May 2024 · 2 mos          Remote",
+        "Apr 2024 - May 2024 · 1 mos          Remote",
   ),
   Expr(
-    titel: "Web Development Intern",
+    titel: "Web Developer Intern",
     company: "Prodigy Infotech",
     durs:
         "Mar 2024 to Mar 2024 · 1 mos          Remote",
   ),
   Expr(
-    titel: "Web Development Intern",
+    titel: "Web Developer Intern",
     company: "Codsoft",
     durs:
-        "Jan 2024 to Feb 2024 · 2 mos          Remote",
+        "Jan 2024 to Feb 2024 · 1 mos          Remote",
   ),
   
 ];
